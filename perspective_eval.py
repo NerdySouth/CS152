c04574e2d5d0ab769c095f54ea39f20a2e9c14d4
Yoni Lerner <yonilern@gmail.com>
2020-02-13 16:32:50 -0800

    Added data and processing function

--- perspective_eval.py
+++ perspective_eval.py
@@ -5,11 +5,14 @@ on a snippet of text.
 
 import requests
 import json
+import pandas as pd
+from tqdm import tqdm
+import time
 
 def eval_text(text):
 	# This is the URL which Perspective API requests go to.
 	PERSPECTIVE_URL = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze'
-	key = "AIzaSyBT0no02hmXlqQIubI9wjeX7QAdSrfp_wQ"; # TODO: fill this in with your Perspective API Key!
+	key = "AIzaSyBT0no02hmXlqQIubI9wjeX7QAdSrfp_wQ";
 
 	url = PERSPECTIVE_URL + '?key=' + key
 	data_dict = {
@@ -23,10 +26,34 @@ def eval_text(text):
 	response = requests.post(url, data=json.dumps(data_dict))
 	response_dict = response.json()
 
-	# Print the entire response dictionary.
-	print("\"" + text + "\"")
-	print(json.dumps(response_dict, indent=4))
+	return response_dict["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
 
 
-# Here you can add code to evaluate particular messages.
-eval_text("As an example: am I toxic?")
+def find_anomalies(tweets, anomalous_above_threshold, threshold=0.5, max_num=5, gui=True):
+	anomalies = []
+	if gui:
+		pbar = tqdm(total=max_num)
+	for i, tweet in enumerate(tweets):
+		score = eval_text(tweet)
+
+		if gui:
+			pbar.set_description("Made %d requests" % (i + 1))
+
+		if (anomalous_above_threshold and score > threshold) or (not anomalous_above_threshold and score < threshold):
+			anomalies.append((tweet, score))
+			if gui:
+				pbar.update(1)
+			if not max_num is None and len(anomalies) >= max_num:
+				break
+
+		time.sleep(1)
+	return anomalies
+
+# https://github.com/t-davidson/hate-speech-and-offensive-language
+df = pd.read_csv("hate-speech-and-offensive-language/data/labeled_data.csv")
+clean = df[df['class'] == 2]['tweet'].to_list()
+dirty = df[df['class'] != 2]['tweet'].to_list()
+
+anomalies = find_anomalies(clean, True, threshold=0.7) + find_anomalies(dirty, False, threshold=0.3)
+print(anomalies)
+
