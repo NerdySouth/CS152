ea8419dfd16192aa67ec30988bd2fe2f18563962
Yoni Lerner <yonilern@gmail.com>
2020-02-16 16:26:36 -0800

    Add retries

--- perspective_eval.py
+++ perspective_eval.py
@@ -8,8 +8,9 @@ import json
 import pandas as pd
 from tqdm import tqdm
 import time
+from random import shuffle
 
-def eval_text(text):
+def eval_text(text, retry=1):
 	# This is the URL which Perspective API requests go to.
 	PERSPECTIVE_URL = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze'
 	key = "AIzaSyBT0no02hmXlqQIubI9wjeX7QAdSrfp_wQ";
@@ -23,8 +24,18 @@ def eval_text(text):
 		'requestedAttributes': { 'TOXICITY': {} },
 		'doNotStore': True
 	}
-	response = requests.post(url, data=json.dumps(data_dict))
-	response_dict = response.json()
+
+	while True:
+		response = requests.post(url, data=json.dumps(data_dict))
+		response_dict = response.json()
+
+		if "attributeScores" in response_dict:
+			break
+		
+		if retry == -1:
+			raise Exception("Invalid reponse from API (probably polling too fast)")
+
+		time.sleep(retry)
 
 	return response_dict["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
 
@@ -46,13 +57,15 @@ def find_anomalies(tweets, anomalous_above_threshold, threshold=0.5, max_num=5, 
 			if not max_num is None and len(anomalies) >= max_num:
 				break
 
-		time.sleep(1)
+		time.sleep(0.5)
 	return anomalies
 
 # https://github.com/t-davidson/hate-speech-and-offensive-language
 df = pd.read_csv("hate-speech-and-offensive-language/data/labeled_data.csv")
 clean = df[df['class'] == 2]['tweet'].to_list()
 dirty = df[df['class'] != 2]['tweet'].to_list()
+shuffle(clean)
+shuffle(dirty)
 
 anomalies = find_anomalies(clean, True, threshold=0.7) + find_anomalies(dirty, False, threshold=0.3)
 print(anomalies)
