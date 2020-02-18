b8d19c6ad77fca0279a0a5c050157023278bccbe
Yoni Lerner <yonilern@gmail.com>
2020-02-17 18:39:23 -0800

    More

--- perspective_eval.py
+++ perspective_eval.py
@@ -10,8 +10,9 @@ import pandas as pd
 from tqdm import tqdm
 import time
 from random import shuffle
+import sys
 
-def eval_text(text, retry=1):
+def eval_text(text, retry=1, attribute='TOXICITY'):
 	# This is the URL which Perspective API requests go to.
 	PERSPECTIVE_URL = 'https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze'
 	key = "AIzaSyBT0no02hmXlqQIubI9wjeX7QAdSrfp_wQ"; # TODO: fill this in with your Perspective API Key!
@@ -22,7 +23,7 @@ def eval_text(text, retry=1):
 		'languages': ['en'],
 		# This dictionary specifies which attributes you care about. You are welcome to (and should) add more.
 		# The full list can be found at: https://github.com/conversationai/perspectiveapi/blob/master/2-api/models.md
-		'requestedAttributes': { 'TOXICITY': {} },
+		'requestedAttributes': { attribute: {} },
 		'doNotStore': True
 	}
 
@@ -38,7 +39,7 @@ def eval_text(text, retry=1):
 
 		time.sleep(retry)
 
-	return response_dict["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
+	return response_dict["attributeScores"][attribute]["summaryScore"]["value"]
 
 
 def find_anomalies(tweets, anomalous_above_threshold, threshold=0.5, max_num=5, gui=True):
@@ -62,13 +63,14 @@ def find_anomalies(tweets, anomalous_above_threshold, threshold=0.5, max_num=5, 
 	return anomalies
 
 # https://github.com/t-davidson/hate-speech-and-offensive-language
-# df = pd.read_csv("hate-speech-and-offensive-language/data/labeled_data.csv")
-# clean = df[df['class'] == 2]['tweet'].to_list()
-# dirty = df[df['class'] != 2]['tweet'].to_list()
-# shuffle(clean)
-# shuffle(dirty)
-# anomalies = find_anomalies(clean, True, threshold=0.7) + find_anomalies(dirty, False, threshold=0.3)
-# print(anomalies)
+def run1():
+	df = pd.read_csv("hate-speech-and-offensive-language/data/labeled_data.csv")
+	clean = df[df['class'] == 2]['tweet'].to_list()
+	dirty = df[df['class'] != 2]['tweet'].to_list()
+	shuffle(clean)
+	shuffle(dirty)
+	anomalies = find_anomalies(clean, True, threshold=0.7) + find_anomalies(dirty, False, threshold=0.3)
+	print(anomalies)
 
 # https://github.com/aitor-garcia-p/hate-speech-dataset
 def read_data():
@@ -81,6 +83,8 @@ def read_data():
 	bad_fileID = df[df['label'] == 'hate']['file_id'].to_list()
 
 	for filename in os.listdir(path + 'all_files/'):
+		if os.path.splitext(filename)[1] != '.txt':
+			continue
 		file = open(path + 'all_files/' + filename)
 		text = file.read()
 		if os.path.splitext(filename)[0] in good_fileID:
@@ -92,8 +96,40 @@ def read_data():
 
 	return comments_good, comments_bad
 
-clean2, dirty2 = read_data()
-shuffle(clean2)
-shuffle(dirty2)
-anomalies2 = find_anomalies(clean2, True, threshold=0.7) + find_anomalies(dirty2, False, threshold=0.3)
-print(anomalies2)
+def run2():
+	clean2, dirty2 = read_data()
+	shuffle(clean2)
+	shuffle(dirty2)
+	anomalies2 = find_anomalies(clean2, True, threshold=0.7) + find_anomalies(dirty2, False, threshold=0.3)
+	print(anomalies2)
+
+def run3(attribute=None):
+	clean, dirty = [], []
+	path = "anomalies_made/"
+	for filename in os.listdir(path):
+		if os.path.splitext(filename)[1] != '.txt':
+			continue
+		with open(path + filename, 'r') as f:
+			text = f.read()
+			if int(os.path.splitext(filename)[0][4:]) % 2 == 0: # even numbered files are dirty examples
+				dirty.append(text)
+			else:
+				clean.append(text)
+
+	for tweet in clean + dirty:
+		print(tweet, eval_text(tweet, attribute=attribute))
+		time.sleep(0.5)
+
+if __name__ == "__main__":
+	toRun = sys.argv[1]
+	if toRun == "1":
+		run1()
+	elif toRun == "2":
+		run2()
+	elif toRun == "3":
+		attribute = None
+		if len(sys.argv) > 2:
+			attribute = sys.argv[2]
+		run3(attribute=attribute)
+	else:
+		print("Invalid argument")
